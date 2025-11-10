280c102685180996d0337291a305fe5a1f97106e
Mubashir Hassan <mubashirandco@gmail.com>
2025-11-10 15:57:39 +0700

    Upload 17 files via GitHub Uploader by Mubashir Hassan

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Services from './components/Services';
+import Features from './components/Features';
+import Pricing from './components/Pricing';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+      <Services />
+      <Features />
+      <Pricing />
+      <About />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
